13a28b1e7031d1f21e8e530de3b64bd41169bf1f
Pi <jakubj1406@gmail.com>
2026-03-18 17:56:27 +0100

    bit operations, small primes

--- miller.py
+++ miller.py
@@ -1,5 +1,7 @@
 import secrets as s
 
+_SMALL_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251]
+
 def stringToInt(string):
     digit_list = []
     for char in string:
@@ -13,40 +15,40 @@ def stringToInt(string):
         power -= 1
     return number
 
-def _millerRabinTest(n, base):
+def _millerRabinTest(n, base=2):
     d = n - 1
-    s = 0
-    
-    while (d % 2 == 0):
-        d = d//2
-        s += 1
+    r = 0
+    while (d & 1) == 0:  # Bitwise check for even
+        d >>= 1  # Bitwise divide by 2
+        r += 1
     
     x = pow(base, d, n)
     if (x == 1 or x == n - 1):
         return True
     
-    for _ in range(s - 1):
+    for _ in range(r - 1):
         x = pow(x, 2, n)
         
         if (x == n-1):
             return True
     return False
 
-def mil(n, times):
+def mil(n, times=16):
     if (n < 2):
         return False
-    if (n in [2, 3, 5, 7]):
-        return True
-    if (n % 2 == 0):
-        return False
     
-    test = [(s.randbelow(n-4) + 2) for _ in range(times)]
+    # Trial division against small primes for quick composite rejection
+    for p in _SMALL_PRIMES:
+        if n % p == 0:
+            return n == p  # True only if n is the prime itself
     
-    if all([_millerRabinTest(n, i) for i in test]):
-        return True
-    return False
+    for _ in range(times):
+        base = s.randbelow(n - 3) + 2  # Random in [2, n-2]
+        if not _millerRabinTest(n, base):
+            return False
+    return True
 
-def nextPrime(n, times):
+def nextPrime(n, times=16):
     i = n
     if (i % 2 == 0):
         i += 1
@@ -55,6 +57,3 @@ def nextPrime(n, times):
         if mil(i, times):
             return i
         i += 2
-
-        
-        
